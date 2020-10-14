ea87f5043dd49d378ef0edfb8bf8011157432004
Mikhail-MM <leakymee@bu.edu>
2020-10-14 03:31:54 -0400

    Add react-hook-form and some simple validations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 
-function App() {
+function LoginForm() {
+  const { register, errors, getValues, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
-  <div className="App h-full flex justify-center items-center">
     <div className="w-full max-w-md bg-gray-800" >
-      <form action="" className=" bg-white shadow-md rounded px-8 py-8 pt-8">
+      <form onSubmit={handleSubmit(onSubmit)} className=" bg-white shadow-md rounded px-8 py-8 pt-8">
         <div className="px-4 pb-4">
           <label htmlFor="email" className="text-sm block font-bold  pb-2">EMAIL ADDRESS</label>
-          <input type="email" name="email" id="" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 " placeholder="Johnbull@example.com"/>
+          <input ref={register({ required: { value: true, message: "Email Is Required!"} })}type="email" name="email" id="" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 " placeholder="Johnbull@example.com"/>
+          { errors?.email?.message }
         </div>
         <div  className="px-4 pb-4">
           <label htmlFor="password" className="text-sm block font-bold pb-2">PASSWORD</label>
-          <input type="password" name="email" id="" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300" placeholder="Enter your password"/>
+          <input ref={register({ required: { value: true, message: "Password Is Required!"} })}type="password" name="password" id="" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300" placeholder="Enter your password"/>
+          { errors?.password?.message }
         </div>
         <div>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">Sign In</button>
+          <input type="submit" className="btn" value="Sign In" />
         </div>
       </form>
     </div>
-  </div>
   );
+}
+
+function RegisterForm() {
+  const { register, errors, handleSubmit, getValues } = useForm({
+    //reValidateMode: "onSubmit",
+    criteriaMode: "all"
+  });
+  const onSubmit = (data) => console.log(data);
+
+  useEffect(() => {
+    console.log("Effects yey")
+    console.log(errors);
+  });
+  
+  return (
+    <div className="w-full max-w-md bg-gray-800" >
+      <form onSubmit={handleSubmit(onSubmit)} className=" bg-white shadow-md rounded px-8 py-8 pt-8">
+        <div className="px-4 pb-4">
+          <label htmlFor="email" className="text-sm block font-bold pb-2">EMAIL ADDRESS</label>
+          <input ref={register({ required: { value: true, message: "Email Is Required!"} })} type="email" name="email" id="" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 " placeholder="Johnbull@example.com"/>
+          { errors?.email?.message }
+        </div>
+        <div  className="px-4 pb-4">
+          <label htmlFor="password" className="text-sm block font-bold pb-2">PASSWORD</label>
+          <input ref={register({ required: { value: true, message: "Password Is Required!"}  })} type="password" name="password" id="" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300" placeholder="Enter your password"/>
+          { errors?.password?.types?.required }
+        </div>
+        <div  className="px-4 pb-4">
+          <label htmlFor="password" className="text-sm block font-bold pb-2">CONFIRM PASSWORD</label>
+          <input 
+            ref={register({ 
+              required: { 
+                value: true, 
+                message: "Confirmed Password Is Required!"
+              }, 
+              validate: { 
+                matchPW: value => value === getValues().password || "Password must match!"
+              } 
+            })} 
+              type="password" 
+              name="confirmPassword" 
+              id="" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300" placeholder="Confirm your password"
+            />
+            
+          { errors?.password?.types?.required }
+        </div>
+        <div>
+          <input type="submit" className="btn" value="Register" />
+        </div>
+      </form>
+    </div>
+  )
+}
+function App() {
+  return <div className="App h-full flex justify-center items-center">
+    <LoginForm />
+    <RegisterForm />
+  </div>
 }
 
 export default App;
